06a2724695cf0d8e56462a2555a3adc0a4aab353
Pierre Larochelle <pierrebombay@gmail.com>
2013-02-25 20:01:52 -0800

    Removing slime (wasn't being used)

--- config.el
+++ config.el
@@ -182,13 +182,3 @@
           (rename-buffer new-name)
           (set-visited-file-name new-name)
           (set-buffer-modified-p nil))))))
-
-;;; This was installed by package-install.el.
-;;; This provides support for the package system and
-;;; interfacing with ELPA, the package archive.
-;;; Move this code earlier if you want to reference
-;;; packages in your .emacs.
-(when
-    (load
-     (expand-file-name "~/.emacs.d/elpa/package.el"))
-  (package-initialize))
