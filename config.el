da0dbf91f6ccc4c521e24060c042b51117e27b20
Pierre Larochelle <pierrebombay@gmail.com>
2014-10-17 07:24:39 -0700

    Adding a function to uniquefy lines

--- config.el
+++ config.el
@@ -281,6 +281,22 @@
           (set-visited-file-name new-name)
           (set-buffer-modified-p nil))))))
 
+(defun uniquify-all-lines-region (start end)
+  "Find duplicate lines in region START to END keeping first occurrence."
+  (interactive "*r")
+  (save-excursion
+    (let ((end (copy-marker end)))
+      (while
+          (progn
+            (goto-char start)
+            (re-search-forward "^\\(.*\\)\n\\(\\(.*\n\\)*\\)\\1\n" end t))
+        (replace-match "\\1\n\\2")))))
+
+(defun uniquify-all-lines-buffer ()
+  "Delete duplicate lines in buffer and keep first occurrence."
+  (interactive "*")
+  (uniquify-all-lines-region (point-min) (point-max)))
+
 (load-file-in-site-lisp "shell-utils.el")
 
 ;;; local stuff
